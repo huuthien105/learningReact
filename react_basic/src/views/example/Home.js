3f07397e0f90daea41ee840ab066e007d82996b1
Thien Tran <huuthientran.1005@gmail.com>
2023-04-08 17:48:18 +0700

    Redirect chuyen trang

--- react_basic/src/views/example/Home.js
+++ react_basic/src/views/example/Home.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
 class Home extends React.Component {
 
+    componentDidMount() {
+        console.log('>> did mount')
+        setTimeout(() => {
+            this.props.history.push('/todo')
+        }, 5000)
+    }
+
     render() {
+        console.log('>> home render')
         return (
             <div>
                 Hello word for home page
@@ -11,4 +20,5 @@ class Home extends React.Component {
     }
 }
 
-export default Home;
+// HOC: higher order component
+export default withRouter(Home);
